89c375a8eb40a4639ffeaa68f6f5e4bb47dae82e
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-12-08 19:22:27 +0530

    add to server

--- components/featured_hero/FeaturedPost.jsx
+++ components/featured_hero/FeaturedPost.jsx
@@ -4,28 +4,28 @@ import panda from '../../assets/images/panda.png';
 
 import { AiOutlinePicRight } from "react-icons/ai";
 
-function FeaturedPost({data}) {
+function FeaturedPost({ data }) {
     console.log('data =', data)
     return (
         <Link href={`/single-news/${data[0]['slug']}`}>
             <div className="sm:h-[471px] w-[95vw] sm:w-[635px] bg-white  rounded-[6px] my-2 sm:my-0 cursor-pointer">
-            <div className="sm:h-[325px] w-[95vw] sm:w-[635px] overflow-hidden relative ">
-                <img className="h-[100%]" src={data[0].yoast_head_json.og_image[0].url} alt="" />
-                <div className="absolute z-[10] bottom-2 left-6 flex flex-row justify-start gap-2 items-center drop-shadow-3xl">
-                    <AiOutlinePicRight className="text-[#ffd200] font-bold" />
-                    <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">TV SERIES</div>
+                <div className="sm:h-[325px] w-[95vw] sm:w-[635px] overflow-hidden relative ">
+                    <img className="h-[100%]" src={data[0].yoast_head_json.og_image[0].url} alt="" />
+                    <div className="absolute z-[10] bottom-2 left-6 flex flex-row justify-start gap-2 items-center drop-shadow-3xl">
+                        <AiOutlinePicRight className="text-[#ffd200] font-bold" />
+                        <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">TV SERIES</div>
+                    </div>
+                    <div className='fadeBottom absolute bottom-0 left-0 right-0' />
                 </div>
-                <div className='fadeBottom absolute bottom-0 left-0 right-0' />
+                <div className="pt-[7px] pb-[12px] px-[14px]">
+                    <Link href={`/single-news/${data[0]['slug']}`}>
+                        <h2 className="text-[26px] text-black font-semibold leading-8 font-nunitoSans">{data[0].yoast_head_json.title}</h2>
+                    </Link>
+                    {/* <h2 className="text-[26px] font-semibold leading-8 font-nunitoSans">Kung Fu Panda: The Dragon Knight Season 2 Potential Release Date and More</h2> */}
+                    <p className="text-[16px] font-sans text-[#6d6d6d]">{data[0].yoast_head_json.description}</p>
+                    {/* <p className="text-[16px] font-sans text-[#6d6d6d]">Kung Fu Panda: THe Dragon Knight Season 2 has already not been announced and here we have its Expected Release Data, Cast and possible Plot info</p> */}
+                </div>
             </div>
-            <div className="pt-[7px] pb-[12px] px-[14px]">
-                <Link href={`/single-news/${data[0]['slug']}`}>
-                 <h2 className="text-[26px] font-semibold leading-8 font-nunitoSans">{data[0].yoast_head_json.title}</h2>
-                </Link>
-                {/* <h2 className="text-[26px] font-semibold leading-8 font-nunitoSans">Kung Fu Panda: The Dragon Knight Season 2 Potential Release Date and More</h2> */}
-                <p className="text-[16px] font-sans text-[#6d6d6d]">{data[0].yoast_head_json.description}</p>
-                {/* <p className="text-[16px] font-sans text-[#6d6d6d]">Kung Fu Panda: THe Dragon Knight Season 2 has already not been announced and here we have its Expected Release Data, Cast and possible Plot info</p> */}
-            </div>
-        </div>
         </Link>
     )
 }
--- components/featured_hero/MasonaryItems.jsx
+++ components/featured_hero/MasonaryItems.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 
-function MasonaryItem({data, image, category, icon, title }) {
+function MasonaryItem({ data, image, icon, title }) {
+
     return (
         <Link href={`/single-news/${data['slug']}`}>
-        <div className="bg-white w-[95vw] sm:w-[310px] h-[90px] sm:h-[231px] rounded-[3px] flex sm:flex-col gap-x-2 flex-row overflow-hidden cursor-pointer">
-            <div className="h-[100%] sm:h-[175px] w-[320px] sm:w-[100%] overflow-hidden relative">
-                <img src={image} alt="" className="w-[100%] h-[100%]" />
-                <div className="absolute z-[10] bottom-2 sm:left-2  left-2 flex flex-row justify-start gap-2 items-center drop-shadow-3xl ">
-                    <img src={icon} width={15} height={15} className="text-[#ffd200] font-bold drop-shadow-3xl " alt="" />
-                    <div className="capitalize text-[10px] sm:text-[12px] text-[#ffd200] drop-shadow-3xl  font-nunitoSans">{category}</div>
+            <div className="bg-white w-[95vw] sm:w-[310px] h-[90px] sm:h-[231px] rounded-[3px] flex sm:flex-col gap-x-2 flex-row overflow-hidden cursor-pointer">
+                <div className="h-[100%] sm:h-[175px] w-[320px] sm:w-[100%] overflow-hidden relative">
+                    <img src={image} alt="" className="w-[100%] h-[100%]" />
+                    <div className="absolute z-[10] bottom-2 sm:left-2  left-2 flex flex-row justify-start gap-2 items-center drop-shadow-3xl ">
+                        <img src={icon} width={15} height={15} className="text-[#ffd200] font-bold drop-shadow-3xl " alt="" />
+                        <div className="capitalize text-[10px] sm:text-[12px] text-[#ffd200] drop-shadow-3xl  font-nunitoSans">TV SERIES</div>
+                    </div>
+                    <div className='fadeBottom absolute bottom-0 left-0 right-0' />
                 </div>
-                <div className='fadeBottom absolute bottom-0 left-0 right-0' />
+                <div className="sm:p-[10px] p-[6px]">
+                    <h3 className="text-[16px] sm:text-[18px] sm:leading-5 text-black font-semibold  sm:line-clamp-2 line-clamp-2 font-nunitoSans ">{title}</h3>
+                </div>
             </div>
-            <div className="sm:p-[10px] p-[6px]">
-                <h3 className="text-[16px] sm:text-[18px] sm:leading-5 font-semibold  sm:line-clamp-2 line-clamp-2 font-nunitoSans ">{data.yoast_head_json.title}</h3>
-            </div>
-        </div>
         </Link>
     )
 }
