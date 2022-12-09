cde4c33950f03ee1959bcc173ec50eb8f20bb101
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-12-09 22:38:06 +0530

    add to server

--- components/home/SmallerCardItems.jsx
+++ components/home/SmallerCardItems.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import couplesHug from '../../assets/images/couples_hug.png';
-import videoIcon from '../../assets/icons/video-icon.svg';
+import VideoIcon from '../../assets/icons/VideoIcon';
 import formatDate from '../utils/FormatDate';
 function SmallerCardItems({ data }) {
     console.log("Smaller Card items:", data)
@@ -11,13 +11,14 @@ function SmallerCardItems({ data }) {
                     <img className="w-[100%] h-[100%]" src={data?._embedded["wp:featuredmedia"][0].link} alt="" />
                     <div className="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-t from-[#31313193] rounded-md"></div>
                     <div className="absolute  z-[10] bottom-2 sm:left-2  left-2 flex flex-row justify-start gap-1 items-center drop-shadow-3xl ">
-                        <img src={videoIcon} width={15} height={15} className="text-[#ffd200] font-bold" alt="" />
-                        {/* <div className="capitalize text-[8px] text-[#ffd200] font-nunitoSans  font-bold">NOVEMBER 2, 2022</div> */}
+                        {/* <img src={videoIcon} width={15} height={15} className="text-[#ffd200] font-bold" alt="" /> */}
+                        <VideoIcon width={15} height={15} className="text-[#ffd200] font-bold" alt="" />
+                        {/* <div className="capitalize text-[8px] text-[#ffd200] font-nunitoSans  font-bold">{formatDate(data?.date).toUpperCase()}</div> */}
                     </div>
                 </div>
                 <div className="relative flex flex-col justify-between">
                     <p className="text-[10px] text-[#bf912d] font-bold">{data?._embedded["wp:term"][0][0].name.toUpperCase()}</p>
-                    <h2 className="text-[#000000] text-[16px] leading-[18px] font-nunitoSans font-semibold">{data?.title.rendered.replace(/&#8217;/g, "'")}
+                    <h2 className="text-[#000000] text-[16px] leading-[18px] font-nunitoSans font-semibold">{data?.title.rendered.replace(/&#8217;/g, "'").substring(0, 45)}
                     </h2>
                     <p className="text-[10px] text-[#737373]">{formatDate(data?.date).toUpperCase()}</p>
                 </div>
