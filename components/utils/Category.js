e54ed32734988b0034510942d0457ca78b916a8c
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-12-09 22:48:13 +0530

    add to server

--- components/utils/Category.js
+++ components/utils/Category.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getCategoryById } from '../../features/categorySlice'
 import axios from 'axios'
 
-const Category = ({id}) => {
+const Category = ({ id }) => {
   const [cateName, setCateName] = useState()
 
   const url = 'https://dailyresearchplot.com/wp-json/wp/v2/categories'
@@ -19,18 +19,18 @@ const Category = ({id}) => {
   }, [id])
 
   console.log(cateName)
-    // const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-    // useEffect(() => {
-    //     dispatch(getCategoryById(id))
-    // }, [id, dispatch])
+  // useEffect(() => {
+  //     dispatch(getCategoryById(id))
+  // }, [id, dispatch])
 
-    // const {categoryById} = useSelector(state => state.category)
+  // const {categoryById} = useSelector(state => state.category)
 
   return (
     <div>
-       <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">Category</div>
-       {/* <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">{cateName.name}</div> */}
+      <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">Category</div>
+      {/* <div className="capitalize text-[12px] text-[#ffd200] drop-shadow-3xl  font-bold">{cateName.name}</div> */}
     </div>
   )
 }
