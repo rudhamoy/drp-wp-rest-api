0dbfa8b8e1503c1332b7dd2fc7fbf32e2634208e
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-12-12 09:59:00 +0530

    re-update hotspot

--- components/home/HotspotCarousel.jsx
+++ components/home/HotspotCarousel.jsx
@@ -89,18 +89,31 @@ const HotspotCarousel = ({ data, hotspotData }) => {
             {/* desktop */}
             <div className="hidden sm:block">
                 <Slider {...settingsDesk} className="px-2 ">
-                    {hotspotData.map((item, index) => (
-                    <HotSpotSliderItem key={index} category="category" image={item._embedded["wp:featuredmedia"][0].link} title={item.title.rendered} />
-                    // <HotSpotSliderItem key={index} category="category" image={item['_embedded']['wp:featuredmedia'][0]['source_url']} title={item.title.rendered} />
-                    ))}
+                    {hotspotData.map((item, index) => {
+                        const image = "https://dailyresearchplot.com" + item._embedded["wp:featuredmedia"][0].media_details?.sizes.thumbnail.source_url
+                        return (
+                            <HotSpotSliderItem 
+                            key={index} 
+                            category={item?._embedded["wp:term"][0][0].name.toUpperCase()} 
+                            image={image} 
+                            title={item.title.rendered} />
+                            )
+                    })}
                 </Slider>
             </div>
             {/* mobile */}
             <div className='sm:hidden'>
                 <Slider {...settingsMobi} className="px-2 ">
-                    {data.map((item, index) => (
-                        <HotSpotSliderItem key={index} category={item.category} image={item.img} title={item.title} />
-                    ))}
+                {hotspotData.map((item, index) => {
+                        const image = "https://dailyresearchplot.com" + item._embedded["wp:featuredmedia"][0].media_details?.sizes.thumbnail.source_url
+                        return (
+                            <HotSpotSliderItem 
+                            key={index} 
+                            category={item?._embedded["wp:term"][0][0].name.toUpperCase()} 
+                            image={image} 
+                            title={item.title.rendered} />
+                            )
+                    })}
                 </Slider>
             </div>
         </div>
