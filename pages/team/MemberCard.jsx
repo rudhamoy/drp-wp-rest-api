69a0917bbb84a2d9100fe07d8e1ec5a89e5d4d40
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-12-30 18:25:39 +0530

    adad

--- pages/team/MemberCard.jsx
+++ pages/team/MemberCard.jsx
@@ -7,15 +7,17 @@ import Link from 'next/link';
 function MemberCard({ data, host }) {
     const [zoom, setZoom] = useState(false);
     // spliting the gravetar url params to update height
-    const gravaterImageUrl = data.avatar_urls["96"]
-    const authorImageLink = gravaterImageUrl.split('?')[0]
+
+    // const gravaterImageUrl = data.avatar_urls["96"]
+
+    // const authorImageLink = gravaterImageUrl.split('?')[0]
     return (
         <>
             <div className="bg-[#dddddd] p-4 w-[16rem] h-[25rem] flex flex-col items-center relative">
                 {/* image container To do: Make this image container scale up on hover */}
-                <div onMouseEnter={() => setZoom(true)} onMouseOut={() => setZoom(false)} className={` bg-gray-500  ${zoom ? " absolute top-0 h-[30rem]  w-[30rem] z-[2] cursor-pointer transition-all duration-500" : "overflow-hidden rounded-[100%] h-[7rem]  w-[7rem]"}`}>
+                <div onMouseEnter={() => setZoom(true)} onMouseOut={() => setZoom(false)} className={` bg-gray-300  ${zoom ? " absolute top-0 h-[30rem]  w-[30rem] z-[2] cursor-pointer transition-all duration-500" : "overflow-hidden rounded-[100%] h-[7rem]  w-[7rem]"}`}>
                     {/* <Image height="auto" width="auto" src={data.avatar_urls["96"]} style={{ objectFit: 'cover' }} /> */}
-                    <img src={authorImageLink + "?s=500"}></img>
+                    <img src=""></img>
                 </div>
                 {/* end image container */}
                 <h3 className="text-center text-[1.2rem] text-[#e20e59] hover:text-gray-700 cursor-pointer">
